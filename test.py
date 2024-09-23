d98a5c18b586565758662aa52b6010c55501dc6d
JPS1217 <jspatinoa@unal.edu.co>
2024-09-23 18:02:16 -0500

    Update test.py
    update

--- test.py
+++ test.py
@@ -18,11 +18,11 @@ if not os.path.exists("countries.csv"):
 dataframe = pd.read_csv("countries.csv")
 dataframe = dataframe.set_index("country")
 
-assert dataframe["United States of America"] == 579
-assert dataframe["China"] == 273
-assert dataframe["India"] == 174
-assert dataframe["United Kingdom"] == 173
-assert dataframe["Italy"] == 112
+assert dataframe.loc["United States of America"] == 579
+assert dataframe.loc["China"] == 273
+assert dataframe.loc["India"] == 174
+assert dataframe.loc["United Kingdom"] == 173
+assert dataframe.loc["Italy"] == 112
 
 if not os.path.exists("map.html"):
     raise FileNotFoundError("File 'map.html' not found")
